8ca66b0de2fe7bf76d895383ba243cb59576f927
Diego Carias <diegocarias503@gmail.com>
2022-06-05 09:49:48 -0600

    Estructura del home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './pages/home/Home';
+import BtnHome from './components/btn/Btnhome';
 
 
 function App() {
   return (
     <div className="App">
-        
+         <Home   />
     </div>
   );
 }
