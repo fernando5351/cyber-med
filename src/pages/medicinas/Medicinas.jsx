8c92370e57af756e5d6cbd767bd634697f2528f1
Josue <hernandzmarlon8@gmail.com>
2022-06-10 17:40:10 -0600

    se agregaron las rutas a los iconos de la seccion de medicinas

--- src/pages/medicinas/Medicinas.jsx
+++ src/pages/medicinas/Medicinas.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/navegacion/Navbar";
 import BtnMedicina from "../../components/btn/BtnMedicina";
 import Barranav from '../../components/navegacion/Barranav';
 import '../../css/medicinas.css'
+import { useNavigate } from 'react-router-dom'
 
 
 function Medicinas() {
