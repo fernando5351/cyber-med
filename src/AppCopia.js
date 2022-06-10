17fe7b81b69bc33600c52ee50efa6225c531c69a
isaac fernando <isaacfernando5351@gmail.com>
2022-06-10 10:22:04 -0600

    actualizacion de la barranav

--- src/AppCopia.js
+++ src/AppCopia.js
@@ -1,17 +1,13 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
-import Medicinas from './pages/medicinas';
-import Login from './pages/login';
 
 const App = () => {
   return (
     <Router>
       <Suspense fallback={<p>Cargando...</p>}>
         <Routes>
-          <Route path="/" name='Login' element={<Login />} />
-          <Route path="/home" name='home' element={<Home />} />
-          <Route path="/medicinas" name='medicinas' element={<Medicinas />} />
+          <Route path="/" name='home' element={<Home />} />
         </Routes>
       </Suspense>
     </Router>
