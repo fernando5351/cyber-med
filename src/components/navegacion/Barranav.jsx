1bcbbb5e92ab064c4d215fbd44a53fc3df3ad679
isaac fernando <isaacfernando5351@gmail.com>
2022-06-10 10:15:45 -0600

    cambio de la barra de botones

--- src/components/navegacion/Barranav.jsx
+++ src/components/navegacion/Barranav.jsx
@@ -3,39 +3,15 @@ import '../../css/barranav.css'
 import Retro from '../../icon/Vista/retroceder.png';
 import Agregar from '../../icon/Vista/agregar.png';
 
-<<<<<<< HEAD
 function Barranav() {
   return (
+    // este es el que deben elegir
     <div className="contenedor-barra">
       <div className="retro">
         <img
           className='atras'
           src={Retro}
           alt='' />
-=======
-function Barranav(props) {
-    return(
-      <div className='contenedor-barra'>
-          <div className='contenedor-btnretro'>
-             <a class="waves-effect waves-light btn-large buttonB">
-             <img
-             className='icoV' 
-             src={Retro} 
-             alt="retroceder" />
-             </a>
-          </div>
-          <div className='contenedor-titulo'>
-             <h1>HOLA MUNDO</h1>
-          </div>
-          <div className='contenedor-btnadd'>
-            <a class="waves-effect waves-light btn-large buttonB">
-                <img 
-                className="icoV"
-                src={Agregar} 
-                alt="Agregar" />
-            </a>
-          </div>
->>>>>>> 96ac131965c2a8c87cdc52d5bcda6d40b5ad3907
       </div>
       <div className="agregar">
         <img
