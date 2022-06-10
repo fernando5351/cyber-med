d5e252faa52b39fd2e0325a93ec4a2e8fafd4fc5
Diego Carias <diegocarias503@gmail.com>
2022-06-10 09:49:45 -0600

    Arreglo de nav

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Home from './pages/home/Home';
+import BtnHome from './components/btn/Btnhome';
+import Navbar from './components/navegacion/Navbar';
 
 function App() {
     return(
         <div className='App'>
-           
+           <Navbar />
         </div>
     );
 }
