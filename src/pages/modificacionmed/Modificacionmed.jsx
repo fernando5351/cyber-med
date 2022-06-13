111a7a22afe7968e7e3a5200757e1a1cf93da0e7
Alejandra593 <alejandra0abigail@gmail.com>
2022-06-13 17:24:33 -0600

    actualizacion pagina modificacionmed

--- src/pages/modificacionmed/Modificacionmed.jsx
+++ src/pages/modificacionmed/Modificacionmed.jsx
@@ -1,23 +1,25 @@
 import React from "react";
 import Navbar from '../../components/navegacion/Navbar';
 import '../../css/modificacionmed.css'
+import imgsubir from '../../icon/Create/subir.png';
 
 function Modificacionmed() {
-    return(
-        <div>
-            <Navbar />
-               <div className='contenedor-modificacion'>
-                <div className='contenedor-img'>
-                </div>
-               <div className='contenedor-dato'>
-                <img src="" alt="" />
-                 </div>
-            </div>
-            <div className='content-info'>
-            <div className='contenedor-info'>
-            </div>
-            </div>
-        </div>  
+    return (
+      <div>
+        <Navbar />
+        <div className="contenedor-modificacion">
+          <div className="contenedor-img">
+            <a class="waves-effect waves-light btn butonmod">
+              <img className="subiricon" src={imgsubir} alt="" />
+            </a>
+          </div>
+          <div className="contenedor-dato">
+          </div>
+        </div>
+        <div className="content-info">
+          <div className="contenedor-info"></div>
+        </div>
+      </div>
     );
 }
 
