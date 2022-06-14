f1ad52f9b556a340af1886a0edbc15efa2f3a8b2
Diego Carias <diegocarias503@gmail.com>
2022-06-14 15:07:59 -0600

    Arreglo de la nav hover eliminado

--- front/src/components/btn/Btnhome.jsx
+++ front/src/components/btn/Btnhome.jsx
@@ -7,18 +7,18 @@ function Btnhome() {
     <div className='row content'>
       <form className='col s12 content'>
          <div className='input-field col s3 content'>
-             <select className='selectores'>
-                     <option value='' disabled selected>ADMINISTRACION</option>
+             <select id='selector'>
+                     <option value='' disabled selected>USO</option>
                      <option value='1'>Option 1</option>
-                     <option value='2'>Option 4</option>
+                     <option value='2'>Option 2</option>
                      <option value='3'>Option 3</option>
              </select>
          </div>
          <div className='input-field col s3 content'>
-             <input placeholder='NOMBRE' id='first_name' type='text' class='validate  entradatext' />             
+             <input placeholder='NOMBRE' id='Nombrein' type='text' class='validate  entradatext' />             
          </div>
          <div className='input-field col s3 content'>
-         <select>
+         <select id='selector'>
                      <option value='' disabled selected>USO</option>
                      <option value='1'>Option 1</option>
                      <option value='2'>Option 2</option>
@@ -43,7 +43,6 @@ function Btnhome() {
 }
 
 export default Btnhome;
-
 
 
 
