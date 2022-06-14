7a294fd55a0c30cddf5bb0852b4d24c3aecb195d
KarlaPocasangre <6373543@clases.edu.sv>
2022-06-14 10:50:50 -0600

    iniaciando vista med

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Medicinas from './pages/medicinas';
-import Login from './pages/login'
-import Home from './pages/home'
-import Navbar from './components/navegacion/Navbar'
-
-
+import Vistamed from './pages/vistamed/VistaMed.jsx'
 
 const App = () => {
   return (
     <Router>
       <Suspense fallback={<p>Cargando...</p>}>
         <Routes>
-        <Route path="/" name='Login' element={<Login />} />
-          <Route path="/home" name='home' element={<Home />} />
-          <Route path="/medicinas" name='medicinas' element={<Medicinas />} />
+        
+          <Route path="/" name='vistamed' element={<Vistamed />} />
+          
         </Routes>
       </Suspense>
     </Router>
--- src/pages/vistamed/VistaMed.jsx
+++ src/pages/vistamed/VistaMed.jsx
@@ -1,17 +1,24 @@
 import React from "react";
 import Navbar from "../../components/navegacion/Navbar";
-import Barranav from "../../components/navegacion/Barranav";
 import '../../css/vistamed.css'
 
 function Vistamed() {
     return(
       <div className='contenedor-vistamed'>
         <Navbar />
-          <div className='contenedor-principal'>
-            <Barranav />
-            <h1>VISTA MED</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut accusamus architecto delectus sequi tempore quia nihil, voluptates eum unde, assumenda at officia similique quae? Aliquam saepe voluptates distinctio quasi et!</p>
+          <div className='contenedor-imagen'>
+
           </div>
+          <div className='contenedor-marca'>
+          </div>
+          <div className='contenedor-descripcion'>
+
+          <input type="button" id="Eliminar" value="Eliminar" className='Eliminar'></input>
+          <input type="button" id="editar" value="Editar" className='Editar'></input>
+          </div>
+          
+
+      
       </div>
     );
 }
