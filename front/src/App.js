7230889b0dceae51b5b0109dee21894cc5fb0f87
Diego Carias <diegocarias503@gmail.com>
2022-06-15 07:12:50 -0600

    BtnHome mejorada 2.0

--- front/src/App.js
+++ front/src/App.js
@@ -4,16 +4,15 @@ import Login from './pages/login';
 import Home from './pages/home'
 import Medicinas from './pages/medicinas';
 import Agregar from './pages/agregar-tipo'
+import Vistauso from './pages/vistauso/Vistauso';
 
 const App = () => {
   return (
     <Router>
       <Suspense fallback={<p>Cargando...</p>}>
         <Routes>
-          <Route path="/" name='Login' element={<Login />} />
-          <Route path="/home" name='home' element={<Home />} />
-          <Route path="/medicinas/agregar" element={<Agregar />} />
-          <Route path="/medicinas" name='medicinas' element={<Medicinas />} />
+          <Route path="/home" name='Home' element={<Home />} />
+          
         </Routes>
       </Suspense>
     </Router>
