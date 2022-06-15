a0d3f52a3f4bb9f7a7a27045987a1315c9f5cb9a
isaac fernando <isaacfernando5351@gmail.com>
2022-06-15 07:14:01 -0600

    tabla de medicinas

--- front/src/pages/agregar-tipo/agregar.jsx
+++ front/src/pages/agregar-tipo/agregar.jsx
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Navbar from '../../components/navegacion/Navbar'
+import Agregar from '../../icon/Vista/agregar.png'
 import '../../css/Add-medicines.css'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 
-const mod = () => {
+function Mod() {
+    const [consumo, setConsumo] = useState([])
+    const loadData = () => {
+        axios.get('http://localhost:4000/medicinas')
+            .then(result => {
+                setConsumo(result.data)
+            })
+    }
+    useEffect(loadData, [])
+    const navigate = useNavigate();
+    const add = () => {
+        navigate('/medicinas/agregar/categoria')
+    }
     return (
         <div className='container-add'>
             <Navbar />
             <div className="container-tab">
                 <div className="container-btn">
-
+                    <img
+                        className='annadir'
+                        src={Agregar}
+                        alt=''
+                        onClick={add} />
                 </div>
                 <div className="container-table">
                     <table className='centered'>
@@ -19,18 +38,24 @@ const mod = () => {
                                 <th className='border-table-rig'>ACCION</th>
                             </tr>
                         </thead>
-                        <tbody className='alt'>
-                            <tr className='background'>
-                                <td className='border-table'>1</td>
-                                <td>Intramuscular</td>
-                                <td className='border-table-rig'>Eliminar/Editar</td>
-                            </tr>
+                        <tbody className='table'>
+                            <>
+                                {
+                                    consumo.map((consumo) => (
+                                        <tr className='background'>
+                                            <td>{consumo.id}</td>
+                                            <td>{consumo.tipo_consumo}</td>
+                                            <td></td>
+                                        </tr>
+                                    ))
+                                }
+                            </>
                         </tbody>
                     </table>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
-export default mod
+export default Mod
