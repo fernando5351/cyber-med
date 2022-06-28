eebdf38040a668a110bda5b1f75429ea6a2d358e
Diego503jr <diegocarias503@gmail.com>
2022-06-27 23:41:13 -0600

    Fusionar las tres pantallas

--- back/index.js
+++ back/index.js
@@ -1,41 +1,43 @@
-const express = require('express')
-const cors = require('cors')
-const mysql  = require('mysql2')
-const app = express()
-app.use(cors())
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2");
+const app = express();
+app.use(cors());
 
 const credenciales = {
-    host: "localhost",
-    user: "schiffer",
-    password: "Fernando-19@20",
-    database: "db_medicines"
-}
+  host: "localhost",
+  user: "Xoxo503",
+  password: "Hernandez503jr",
+  database: "db_medicines",
+};
 
-app.get('/medicinas', (req, res) => {
-    var connection = mysql.createConnection(credenciales)
-    connection.query('SELECT * from tipo_consumo', (error, resultado) => {
-        if (error) {
-            res.status(500).send(error)
-        } else {
-            res.status(200).send(resultado)
-        }
-    })
-    connection.end()
-})
+app.get("/medicinas", (req, res) => {
+  var connection = mysql.createConnection(credenciales);
+  connection.query("SELECT * from tipo_consumo", (error, resultado) => {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.status(200).send(resultado);
+    }
+  });
+  connection.end();
+});
 
-app.get('/productos',(req,res)=>{
-    var connection = mysql.createConnection(credenciales)
-    // En esta linea establecemos la conexion de la tabla productos, de la tabla tipo consumo, y de la tabla tipo medicamentos para que se muestten en el home 
-    connection.query('select productos.id, productos.nombre, productos.descripcion, productos.precios, productos.cant_gramos, tipo_consumo.tipo_consumo, tipo_medicamento.tipo_uso from productos,tipo_consumo,tipo_medicamento where tipo_consumo.id=productos.id_tipo_uso and tipo_medicamento.id=id_tipo_uso;', (error, resultado)=>{
-        if (error) {
-            res.status(500).send(error)
-        }else{
-            res.status(200).send(resultado)
-        }
-    })
-    connection.end()
-})
-
+app.get("/productos", (req, res) => {
+  var connection = mysql.createConnection(credenciales);
+  // En esta linea establecemos la conexion de la tabla productos, de la tabla tipo consumo, y de la tabla tipo medicamentos para que se muestten en el home
+  connection.query(
+    "select productos.id, productos.nombre, productos.descripcion, productos.precios, productos.cant_gramos, tipo_consumo.tipo_consumo, tipo_medicamento.tipo_uso from productos,tipo_consumo,tipo_medicamento where tipo_consumo.id=productos.id_tipo_uso and tipo_medicamento.id=id_tipo_uso;",
+    (error, resultado) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.status(200).send(resultado);
+      }
+    }
+  );
+  connection.end();
+});
 
 // app.post('/login', (req, res) => {
 //     const { email, password } = req.body
@@ -55,4 +57,4 @@ app.get('/productos',(req,res)=>{
 //     conexion.end
 // })
 
-app.listen(4000, ()=> console.log('this is a servidor'))
+app.listen(4000, () => console.log("this is a servidor"));
--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,37 @@
-import React, { Suspense } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './pages/login';
-import Home from './pages/home';
-import Medicinas from './pages/medicinas';
-import Formuso from './components/forms/formtipouso';
-import Agregar from './pages/agregar-tipo'
-import Vistauso from './pages/vistauso/Vistauso';
-import Modificacionmed from './pages/modificacionmed/Modificacionmed';
-import Proveedor from './pages/proveedor/Proveedor';
+import React, { Suspense } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import Home from "./pages/home";
+import Medicinas from "./pages/medicinas";
+import Formuso from "./components/forms/formtipouso";
+import Agregar from "./pages/agregar-tipo";
+import Vistauso from "./pages/vistauso/Vistauso";
+import Modificacionmed from "./pages/modificacionmed/Modificacionmed";
+import Proveedor from "./pages/proveedor/Proveedor";
 
 const App = () => {
   return (
     <Router>
-      <Suspense fallback={<p>Cargando...</p>}>  
+      <Suspense fallback={<p>Cargando...</p>}>
         <Routes>
-          <Route path="/" name='Login' element={<Login />} />
-          <Route path="/home" name='Home' element={<Home />} />
-          <Route path="/medicinas" name='medicinas' element={<Medicinas />} />
-          <Route path="/medicinas/agregar" name='agregarTipo' element={<Agregar />} />
-          <Route path="/medicinas/agregar/categoria" name='Home' element={<Formuso />} />
-<<<<<<< HEAD
-          <Route path="/medicinas/editar/categoria" name='Home' element={<Formuso />} />
-          <Route path="/vistauso" name='Home' element={<Vistauso />} />
-=======
-          <Route path="/modificacionmed" name='Home' element={<Modificacionmed />} />
->>>>>>> diego
+          <Route path="/" name="Login" element={<Login />} />
+          <Route path="/home" name="Home" element={<Home />} />
+          <Route path="/medicinas" name="medicinas" element={<Medicinas />} />
+          <Route
+            path="/medicinas/agregar"
+            name="agregarTipo"
+            element={<Agregar />}
+          />
+          <Route
+            path="/medicinas/agregar/categoria"
+            name="Home"
+            element={<Formuso />}
+          />
+          <Route
+            path="/modificacionmed"
+            name="Home"
+            element={<Modificacionmed />}
+          />
         </Routes>
       </Suspense>
     </Router>
