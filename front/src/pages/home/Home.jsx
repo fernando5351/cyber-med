e9ff20ddd83d5479338a264177176217a4abae6e
Diego503jr <diegocarias503@gmail.com>
2022-06-27 21:22:55 -0600

    navbar dropdown terminada de diseño

--- front/src/pages/home/Home.jsx
+++ front/src/pages/home/Home.jsx
@@ -1,22 +1,20 @@
 import Navbar from "../../components/navegacion/Navbar";
-import ContTa from '../../css/contenttabla.module.css'
-import Btnhome from '../../components/btn/Btnhome'
+import ContTa from "../../css/contenttabla.module.css";
+import Btnhome from "../../components/btn/Btnhome";
 import TableHome from "../../components/table/Tablehome";
 
-
 function Home() {
-    return(
-      <div className={ContTa.contenedorMT}>
-          <Navbar />
-          <div className={ContTa.contenedorBtn}>
-          <Btnhome />
-          </div>
-          <div className={ContTa.contenedorTH}>
-          <TableHome />
-          </div>
+  return (
+    <div className={ContTa.contenedorMT}>
+      <Navbar />
+      <div className={ContTa.contenedorBtn}>
+        <Btnhome />
       </div>
-           
-    );
-}  
+      <div className={ContTa.contenedorTH}>
+        <TableHome />
+      </div>
+    </div>
+  );
+}
 
 export default Home;
