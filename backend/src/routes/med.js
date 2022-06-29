8e05c8ee622e1aac841b11d7b39474c5146f1c51
fernando <isaacfernando5351@gmail.com>
2022-06-29 08:33:16 -0600

    implementando el nuevo backend

--- backend/src/routes/med.js
+++ backend/src/routes/med.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../database.js');
 
-router.get('/', (req, res) => {
+
+//trayendo los datos de la tabla
+router.get('/agregar/uso', (req, res) => {
     connection.query('SELECT * FROM tipo_consumo', (err, rows, fields) => {
         if (!err) {
             res.json(rows);
@@ -24,32 +26,38 @@ router.get('/:id', (req, res) => {
         })
 })
 
-router.post('/add', (req, res) => {
+
+// Crear un nuevo uso
+router.post('/add/consumo', (req, res) => {
     const { id, tipo_consumo, estado } = req.body;
-    const query = "INSERT INTO tipo_consumo(tipo_consumo, estado) values (?, ?);";
-    connection.query(query, [id, tipo_consumo, estado], (err, rows, fields) => {
+    console.log(id, tipo_consumo, estado);
+    connection.query("INSERT INTO tipo_consumo(id, tipo_consumo, estado) values (?, ?, ?);", [id, tipo_consumo, estado], (err, rows, fields) => {
         if (!err) {
             res.json({ status: 'tipo de consumo agregado' });
-            console.log("eliminado con exito", { id });
-        } else {
-            console.log(err);
-        }
-    })
-})
-
-router.put('/edit/:id', (req, res) => {
-    const { tipo_consumo, estado } = req.body;
-    const { id } = req.params;
-    const query = "UPDATE tipo_consumo SET tipo_consumo=?, estado=? WHERE id LIKE ?;";
-    connection.query(query, [id, tipo_consumo, estado], (err, rows, fields) => {
-        if (!err) {
-            res.json({ status: "tipo de medicina eliminado" })
+            console.log(`tipo de medicina agregado con exito`);
         } else {
             console.log(err);
         }
     });
 });
 
+
+// Editar un nuevo uso
+router.put('/edit/:id', (req, res) => {
+    const { tipo_consumo, estado } = req.body;
+    const { id } = req.params;
+    const query = "UPDATE tipo_consumo SET id=?, tipo_consumo=?, estado=? WHERE id LIKE ?;";
+    connection.query(query, [id, tipo_consumo, estado, id], (err, rows, fields) => {
+        if (!err) {
+            res.json({ status: "tipo de medicina actualizado" })
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+
+// Eliminar un nuevo uso
 router.delete('/delete/:id', (req, res) => {
     const { id } = req.params;
     connection.query('DELETE FROM tipo_consumo WHERE id = ?', [id], (err, rows, fields) => {
