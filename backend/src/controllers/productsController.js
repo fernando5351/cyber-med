14949020c8be2045fb405ba4ef66be18f5858c50
fernando5351 <isaacfernando5351@gmail.com>
2022-07-24 19:42:21 -0600

    arreglando bug de consulta

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -2,17 +2,17 @@ const { factory, connection } = require('../factory/quey_factory');
 const port = `http://localhost:4000`;
 
 async function getProducts (req, res) {
-    const { nombre } = req.body;
-    console.log(nombre);
-    let query = `SELECT productos.id, productos.nombre, productos.precios, productos.marca, productos.cant_gramos FROM productos WHERE nombre LIKE "${nombre}%";`;
+    const { id } = req.params;
+    console.log(id);
+    let query = `SELECT productos.id, productos.nombre, productos.precios, productos.marca, productos.cant_gramos FROM productos WHERE id LIKE ${id};`;
     const form1 = await factory(query);
 
     //let query = `SELECT productos.id, productos.descripcion, productos.img_url, productos.nombre, productos.precios, productos.marca, productos.cant_gramos, tipo_medicamento.tipo_uso, tipo_consumo.tipo_consumo, productos.cantidad_medicamento FROM productos, tipo_medicamento, tipo_consumo WHERE productos.id_tipo_uso LIKE tipo_medicamento.id &&  productos.id_tipo_consumo LIKE tipo_consumo.id;`;
 
-    let query2 = `SELECT productos.descripcion, tipo_medicamento.tipo_uso, tipo_consumo.tipo_consumo, productos.cantidad_medicamento FROM productos, tipo_medicamento, tipo_consumo WHERE productos.nombre LIKE "${nombre}%" && productos.id_tipo_uso LIKE tipo_medicamento.id &&  productos.id_tipo_consumo LIKE tipo_consumo.id;`;
+    let query2 = `SELECT productos.descripcion, tipo_medicamento.tipo_uso, tipo_consumo.tipo_consumo, productos.cantidad_medicamento FROM productos, tipo_medicamento, tipo_consumo WHERE productos.id LIKE ${id} && productos.id_tipo_uso LIKE tipo_medicamento.id &&  productos.id_tipo_consumo LIKE tipo_consumo.id;`;
     const form2 = await factory(query2);
 
-    let query3 = `SELECT productos.img_url FROM productos WHERE nombre LIKE "${nombre}%"`;
+    let query3 = `SELECT productos.img_url FROM productos WHERE id LIKE ${id}`;
     const getImg = await factory(query3);
 
     res.json({ img: getImg, formFirst: form1, formSecond: form2 });
--- backend/src/routes/productos.js
+++ backend/src/routes/productos.js
@@ -8,7 +8,7 @@ const upload = multer({storage})
 
 const { getProducts, postProduct, editProducts, delProducts } = require('../controllers/productsController')
 
-router.get('/view/products', getProducts);
+router.get('/view/products/:id', getProducts);
 router.post('/create/products', upload.single('file'), postProduct);
 router.put('/edit/productos/:id', upload.single('file'), editProducts);
 router.delete('/delete/products/:id', delProducts);
