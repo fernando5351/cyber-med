3a348dcd809b9f4851010bde4c44b1e469e052ac
fernando5351 <isaacfernando5351@gmail.com>
2022-08-03 20:59:34 -0600

    agregando funcionalidad para eliminar imagenes de clorudinary

--- backend/src/controllers/productsController.js
+++ backend/src/controllers/productsController.js
@@ -28,91 +28,58 @@ async function getProducts(req, res) {
   console.log(query2);
 }
 
-// async function postProduct(req, res) {
-//   const { file, body } = req;
-
-//   //console.log(file, body, nombre, precios, marca, cant_gramos, descripcion, id_tipo_uso, id_tipo_consumo, cantidad_medicamento);
-//   if (file) {
-
-//     let url = `${port}/images/${file.filename}`;
-//     let query = `INSERT INTO productos(img_url,descripcion, id_tipo_uso, id_tipo_consumo, cantidad_medicamento, nombre, precios, marca, cant_gramos) VALUES (${connection.escape(url)},${connection.escape(body.descripcion)}, ${connection.escape(body.id_tipo_uso)}, ${connection.escape(body.id_tipo_consumo)}, ${connection.escape(body.cantidad_medicamento)},${connection.escape(body.nombre)}, ${connection.escape(body.precios)},${connection.escape(body.marca)},${connection.escape(body.cant_gramos)})`;
-//     const postData = await factory(query);
-
-//     res.json({ getData: postData });
-//     console.log(query);
-
-//     const response = await cloudinary.v2.uploader.upload(req.file.path)
-
-//     console.log(`data the file: ${response}`)
-//   }
-// }
-
 async function postProduct (req, res) {
-    const { descripcion, id_tipo_consumo, id_tipo_uso, cantidad_medicamento, nombre, precios, marca, cant_gramos } = req.body
+  const { descripcion, id_tipo_consumo, id_tipo_uso, cantidad_medicamento, nombre, precios, marca, cant_gramos } = req.body
 
-    console.log(req.file);
+  //subiendo imagenes a cloudinary
+  const response = cloudinary.v2.uploader.upload(req.file.path)
 
-    const response = cloudinary.v2.uploader.upload(req.file.path)
-    console.log((await response).public_id)
-    console.log((await response).url)
+  //obtener la direccion y el id de la imagen en cloudinary
+  let route = (await response).url;
+  let name_img = (await response).public_id;
+  let query = `INSERT INTO productos(img_url, name_img, descripcion, id_tipo_uso, id_tipo_consumo, cantidad_medicamento,nombre, precios, marca, cant_gramos) VALUES (${connection.escape(route)}," ${name_img}", "${descripcion}", ${id_tipo_uso}, ${id_tipo_consumo}, ${cantidad_medicamento},"${nombre}", ${precios},"${marca}",${cant_gramos})`;
+  const postData = await factory(query);
 
-    var route = (await response).url;
-    
-    let query = `INSERT INTO productos(img_url,descripcion, id_tipo_uso, id_tipo_consumo, cantidad_medicamento,nombre, precios, marca, cant_gramos) VALUES (${connection.escape(route)},"${descripcion}", ${id_tipo_uso}, ${id_tipo_consumo}, ${cantidad_medicamento},"${nombre}", ${precios},"${marca}",${cant_gramos})`;
-    const postData = await factory(query);
+  res.json({ getData: postData });
 
-    res.json({ getData: postData });
-}
-
-async function delProducts(req, res) {
-  const { id } = req.params;
-
-  //query para obtener el nombre de la imagen
-  let query = `SELECT img_url FROM productos WHERE id LIKE ${id}`;
-  const response = await factory(query);
-
-  //guardamos en una variable el valor de el json
-  var variable = response;
-
-  //desestructuramos lo que obtenemos del json
-  let url = variable[0].img_url.split("/");
-
-  //obtenemos el nombre de la img
-  let dir = url[4];
-
-  //eliminamos el registro
-  let sql = `DELETE FROM productos WHERE id LIKE ${id}`;
-  const delData = await factory(sql);
-
-  res.json({ delData });
-  console.log(sql);
+  //obtener el nombre de la imagen para removerla del server
+  const img = req.file.filename;
+  console.log(` el nombre de la imagen es: ${img}`)
 
   //ruta donde se hubica la imagen
-  let route = path.join(__dirname, `../../public/images/${dir}`);
+  let router = path.join(__dirname, `../../public/images/${img}`);
 
   //eliminamos la imagen por el metodo file system
   try {
-    console.log(response);
-    fs.unlinkSync(route);
-    console.log(` file removed ${dir}`);
+    fs.unlinkSync(router);
+    console.log(` file removed ${router}`);
     //file removed
   } catch (err) {
     console.error(err);
   }
 }
 
+async function delProducts(req, res) {
+  const { id } = req.params;
 
-async function alumnos ( req, res) {
-  const { nombre, apellidos, nie } = req.body;
-  let query = `INSERT INTO alumnos(nombre, apellidos, nie) VALUES ("${nombre}","${apellidos}",${nie})`
-  const response = await factory(query);
+  //obtener el nombre de la imagen
+  let getData = `SELECT name_img FROM productos WHERE id LIKE ${id}`;
+  const data = await factory(getData);
 
-  res.json({response})
+  const dataRes = data
+  console.log(dataRes.name_img)
+  //const name_img = viewData.name_img
+
+  //eliminamos el registro
+  let sql = `DELETE FROM productos WHERE id LIKE ${id}`;
+  const response = await factory(sql)
+
+  res.json(response)
+  //cloudinary.v2.uploader.destroy(name_img)
 }
 
 module.exports = {
   getProducts,
   postProduct,
   delProducts,
-  alumnos
-};
+ };
--- backend/src/routes/productos.js
+++ backend/src/routes/productos.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-//requerimientos para las imagenes
-const storage = require('../../config/multer')
-const multer = require('multer')
-const port = `http://localhost:4000`;
-const upload = multer({storage});
 
-const { getProducts, postProduct, delProducts, alumnos } = require('../controllers/productsController')
+const { getProducts, postProduct, delProducts } = require('../controllers/productsController')
 
 router.get('/view/products/:id', getProducts);
 router.post('/products', postProduct);
-router.post('/alumnos', alumnos);
-router.delete('/delete/products/:id', upload.single('file'), delProducts);
+router.delete('/products/:id', delProducts);
 
 module.exports = router
