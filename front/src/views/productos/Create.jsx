aa4c54b8264ad10c18593824810b2b3e8443a98b
fernando5351 <isaacfernando5351@gmail.com>
2022-08-07 21:08:02 -0600

    formulario productos

--- front/src/views/productos/Create.jsx
+++ front/src/views/productos/Create.jsx
@@ -4,38 +4,35 @@ import vistamed from "../../css/vistamed.module.css";
 import imgsubir from "../../icon/Create/subir.png";
 import select from "../../css/select.module.css";
 import Select from "react-select";
-import { useNavigate } from "react-router-dom";
 
-function Create() {
+function VistaMedy(props) {
   const options = [
     { value: "Opcion 1", label: "Opcion 1" },
     { value: "Opcion 2", label: "Opcion 2" },
     { value: "Opcion 3", label: "Opcion 3" },
   ];
-
-  const navigate = useNavigate();
-
-  const update = () => {
-    navigate(`/medicinas/actualizar`);
-  };
   return (
     <div>
       <Navbar />
       <div className={vistamed.contentPrincipal}>
         <div className={vistamed.contentModificacion}>
           <div className={vistamed.Img}>
-            <buton className={vistamed.butonmed}>
+            <a className={vistamed.butonmed}>
               <img className={vistamed.subiricon} src={imgsubir} alt="" />
-            </buton>
+            </a>
           </div>
           <div className={vistamed.Dato}>
             <form id={vistamed.estiloForm}>
+              <div className={vistamed.contenedorLinea}>
+                <label id={vistamed.color}>NOMBRE :</label>
+                <input id={vistamed.estilo} type="text" />
+              </div>
               <div className={vistamed.contenedorLinea}>
                 <label id={vistamed.color}>PRECIO :</label>
                 <input id={vistamed.estilo} type="text" />
               </div>
               <div className={vistamed.contenedorLinea}>
-                <label id={vistamed.color}>GRAMO :</label>
+                <label id={vistamed.color}>GRAMOS :</label>
                 <input id={vistamed.estilo} type="text" />
               </div>
               <div className={vistamed.contenedorLinea}>
@@ -71,10 +68,14 @@ function Create() {
                   <label id={vistamed.estiloColor}>CANTIDAD :</label>
                   <input id={vistamed.colorInfo} type="text" />
                 </div>
+                <div className={`${vistamed.caja} ${vistamed.estiloBottom}`}>
+                  <label id={vistamed.estiloColor}>LOTE :</label>
+                  <input id={vistamed.colorInfo} type="text" />
+                </div>
               </div>
               <div className={vistamed.containerFormBtn}>
                 <button className={vistamed.btnForm}>Cancelar</button>
-                <button onClick={update} className={vistamed.btnForm}>Actualizar</button>
+                <button className={vistamed.btnForm}>Actualizar</button>
               </div>
             </form>
           </div>
@@ -84,4 +85,4 @@ function Create() {
   );
 }
 
-export default Create;
+export default VistaMedy;
